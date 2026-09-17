57fb782546e76c307b6cb86268efa176169b922b
dominik766 <lkts.dominik@telekimezotur.hu>
2026-09-17 11:40:12 +0200

    atlag

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,8 +1,8 @@
 ﻿using _1;
 
 Auto kek = new Auto(2020, "Focus", "Ford");
-Auto zold = new Auto(3100, "Golf", "Wolkswagen");
-Auto piros = new Auto(2017, "Dacia", "Lodgy");
+Auto zold = new Auto(1992, "Golf", "Wolkswagen");
+Auto piros = new Auto(2018, "Fasz", "Habbal");
 
 List<Auto> garazs = new List<Auto>() 
 { 
@@ -15,3 +15,7 @@ foreach (Auto seged in garazs)
 {
     seged.KiirAdatok();
 }
+
+//atlag szamitas
+double atlag = garazs.Average(x => (double)x.GyartasiEv);
+Console.WriteLine($"Átlag gyártási év: {atlag}");
