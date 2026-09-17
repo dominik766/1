5d122881a373e04e064f3a4e538b58100e87c80a
dominik766 <lkts.dominik@telekimezotur.hu>
2026-09-17 11:16:41 +0200

    konstruktor

---
+++ 1/Auto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1
+{
+    internal class Auto
+    {
+        private int gyartasiEv;
+        public string Modell { get; set; }
+        public string Marka { get; set; }
+        public int GyartasiEv {
+            get { return gyartasiEv; }
+            set {
+                if (value >= 1886 && value <= 2026)
+                {
+                    gyartasiEv = value;
+                }
+                else gyartasiEv = 0;
+            }
+        }
+        public Auto(int ev, string modell, string marka)
+        {
+            GyartasiEv = ev;
+            Modell = modell;
+            Marka = marka;
+        }
+        public void KiirAdatok();
+    } }
--- 1/Program.cs
+++ 1/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("szia cica van gazdád?");
+﻿using _1;
+
+Auto kek = new Auto(2020, "Focus", "Ford");
+Auto zold = new Auto(3100, "Golf", "Wolkswagen");
+
+Console.WriteLine($"kék autó: {kek.Marka} {kek.Modell} gyártási év: {kek.GyartasiEv}");
+Console.WriteLine($"zöld autó: {zold.Marka} {zold.Modell} gyártási év: {zold.GyartasiEv}");
